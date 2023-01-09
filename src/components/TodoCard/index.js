1508cef88e79df319a41d2d1e39836015694f2b2
SunilC <sunilkumarchitikina105@gmail.com>
2023-01-09 15:51:16 +0530

    edit functionality added

--- src/components/TodoCard/index.js
+++ src/components/TodoCard/index.js
@@ -1,8 +1,15 @@
+import { useState } from "react"
 import { MdDelete } from 'react-icons/md'
+import { FiEdit } from 'react-icons/fi'
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 import './index.css'
 
 const TodoCard = ({ todoItem, checkableStatus, deleteTodo }) => {
+    const [edit, setEdit] = useState(false)
     const { id, todo, isChecked } = todoItem
+    const [label, setlabel] = useState(todo)
+
     const onChangeCheckbox = (event) => {
         console.log(event.target.checked)
         checkableStatus(event.target.checked, id)
@@ -12,14 +19,46 @@ const TodoCard = ({ todoItem, checkableStatus, deleteTodo }) => {
         deleteTodo(id)
     }
 
+    const onEdit = (event) => {
+        setlabel(event.target.value)
+    }
+
+    const onSave = () => {
+        setEdit(!edit)
+    }
+
     const todoStatus = isChecked ? "checkbox-complete-label" : "checkbox-label"
 
     return (
         <li class="todo-item-container d-flex flex-row" id="todo1">
             <input type="checkbox" id={id} className="checkbox-input" onChange={onChangeCheckbox} />
-            <div className="label-container d-flex flex-row">
-                <label htmlFor={id} id="label1" className={todoStatus}>{todo}</label>
+            <div className="label-container d-flex flex-row rounded">
+                <label htmlFor={id} id="label1" className={todoStatus}>{edit ? label : todo}</label>
                 <div className="delete-icon-container">
+                    <Popup
+                        modal
+                        trigger={
+                            <button type="button" className="delete-button" onClick={onEdit} id={id}>
+                                <FiEdit className="edit-icon" />
+                            </button>
+                        }
+                        position="top right"
+                    >
+                        {
+                            close => (
+                                <div className="p-3">
+                                    <input type="text" className="form-control" onChange={onEdit} value={label} />
+                                    <br />
+                                    <div>
+                                        <button className="btn btn-primary mr-2" onClick={onSave}>Save</button>
+                                        <button className="btn btn-secondary mr-2" onClick={() => close()}>Close</button>
+
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </Popup>
+
                     <button type="button" className="delete-button" onClick={onDelete} id={id}>
                         <MdDelete className="delete-icon" />
                     </button>
